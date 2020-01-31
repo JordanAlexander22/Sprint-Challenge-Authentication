30f4d44746d0e833213606d53c4cf8aabad2825c
jordan <jordan@jordans-MacBook-Air.local>
2020-01-31 15:26:31 +0700

    authenitcate middleware with token

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,20 @@
   before granting access to the next middleware/route handler
 */
 
-module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+const jwt = require("jsonwebtoken");
+
+module.exports= (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).json({ message: 'You shall not pass!' });
+
+  try {
+    const verified = jwt.verify(
+      token,
+      process.env.TOKEN_SECRET || 'shhh secret'
+    );
+    req.token = verified;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid Token' });
+  }
 };
